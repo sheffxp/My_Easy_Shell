1d365e2e11781904028151de5aa72c8afb0120da
loktev <sheffxp@mail.ru>
2019-07-18 18:20:23 +0400

    v0.0.2 br

--- myeasyshell.sh
+++ myeasyshell.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/usr/sbin
 # задаем переменные
 #-----------------
 vers="For Debian"
-ver="v0.0.2"
+ver="v0.0.2a"
 title="My Easy Shell"
 title_full="$title $ver"
 filename='myeasyshell.sh'
